43d8a0fab3850ef8aa8d947657ded9cd2053fea9
fabrixxm <fabrix.xm@gmail.com>
2018-04-25 22:29:57 +0200

    Use UL for details, fix title margins

--- gamecard.php
+++ gamecard.php
@@ -2,12 +2,10 @@
     <div class="bgg-thumb"><img src="<?php echo $bg->thumbnail?>"></div>
     <div class="bgg-data">
         <h1><a href="https://boardgamegeek.com/boardgame/<?php echo $gameid?>/"><?php echo $bg->name[0]?></a> <span>(<?php echo $bg->yearpublished?>)</span></h1>
-        <div class="bgg-details">
-            <div><?php echo $bg->minplayers." - ".$bg->maxplayers?> Players</div>
-            <div><?php echo $bg->minplaytime." - ".$bg->maxplaytime?> Min</div>
-            <div>Age: <?php echo $bg->age?>+</div>
-        
-        </div>
-        
+        <ul class="bgg-details">
+            <li><?php echo $bg->minplayers." - ".$bg->maxplayers?> Players</li>
+            <li><?php echo $bg->minplaytime." - ".$bg->maxplaytime?> Min</li>
+            <li>Age: <?php echo $bg->age?>+</li>
+        </ul>
     </div>
 </div>
